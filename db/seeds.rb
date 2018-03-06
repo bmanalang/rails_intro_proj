45d0b4670d0e6144819c4f29245585e50fcd0f06
Bernadette Manalang <b_manalang@outlook.com>
2018-03-06 03:29:39 +0000

    Created food items from Food.csv, and new ingredients & beer using Faker.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,16 @@ options = { file_encoding: 'iso-8859-1',
 
 food_names = SmarterCSV.process(csv_foodfile, options)
 
+food_names.each do |f|
+  food = Food.create(f)
 
+  ingredients = Faker::Food.ingredient
+  ingredient = Ingredient.new
+  ingredient.ingredient_name = ingredients
+  ingredient.save
+
+  beer = Faker::Beer.style
+  brew = Beer.new
+  brew.beer_type = beer
+  brew.save
+end
