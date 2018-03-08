2380df3a0a48141fd344db3d1c7314da7c9c8d6f
Bernadette Manalang <b_manalang@outlook.com>
2018-03-08 17:25:57 +0000

    Created About controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'about/index'
+
   get 'welcome/index'
 
   resources :beers
@@ -8,6 +10,6 @@ Rails.application.routes.draw do
 
   get 'search', to: 'search#index', as: 'search'
 
-  root to: 'foods#index'
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
